48beb310a2910dc2389a39099c6ef61efcaa55d0
adrianraszka <43697407+adrianraszka@users.noreply.github.com>
2020-01-22 19:52:26 +0100

    drawing garins

--- multiscale_modeling.py
+++ multiscale_modeling.py
@@ -11,22 +11,30 @@ import numpy as np
 import random
 from statistics import mode
 from statistics import StatisticsError
+import sys
+np.set_printoptions(threshold=sys.maxsize)
 
 class Grain_grow:
-
 
     height = 50 #TODO: tkinter var
     width = 50 #TODO: tkinter var
     image = np.zeros((height, width), dtype=int)
-    number_of_grains = 10 #TODO: tkinter var
+    image_old = np.zeros((height, width), dtype=int)
+    number_of_grains = 5 #TODO: tkinter var
+    
 
     def __init__(self):
 
         # self.testa(self)
         self.seed_random_grains()
-        self.update_status_of_grain()
+        self.copy_image()
+
+        for i in range(1):
+            self.update_status_of_grain()
+
         plt.matshow(self.image)
         plt.show()
+
         
     def seed_random_grains(self):
 
@@ -38,23 +46,34 @@ class Grain_grow:
         # self.image[0, 0] = 11
         return self.image
 
+
     def update_status_of_grain(self):
 
         """This function iterates over every value in matrix and
         if value in 'cell' is equal 0, runs Rules.von_neumann()
         to count most occuring value around that cell using Von Neumann method
         then signs that value to the cell. """
-        for i in range(10):
-            for row, row_tab in enumerate(self.image):
-                for col in range(len(row_tab)):
-                    if self.image[row, col] == 0:
-                        self.image[row, col] = Rules.von_neumann(self, row, col)
 
-        return self.image
+        self.copy_image()
+        
+        for row, row_tab in enumerate(self.image_old):
+            for col in range(len(row_tab)):
+                if self.image_old[row, col] == 0:
+                    self.image[row, col] = Rules.von_neumann(self, row, col)
+    
+        return self.image 
 
-    def testa(self):
-        pass
-        # self.image[0, 0] = 1
+
+    def copy_image(self):
+        
+        for i in range(0, 50): #TODO: VAR
+            for j in range(0, 50): #TODO: VAR
+                self.image_old[i, j] = self.image[i, j]
+
+        print(self.image_old)        
+        return self.image_old
+
+
 
 class Rules:
 
@@ -62,41 +81,32 @@ class Rules:
     def von_neumann(self, row, col):
 
         self.values_of_neighbors = []
-        self.cell_coordinates = []
-        
-        if Grain_grow.image[row, col] == 0:
-            self.cell_coordinates.append([row, col])
 
         if (row-1) < 0: #top
             pass
         else: 
-            self.values_of_neighbors.append(Grain_grow.image[row-1, col])
+            self.values_of_neighbors.append(Grain_grow.image_old[row-1, col])
 
         if (row+1) > Grain_grow.height-2: #bottom
             pass
         else: 
-            self.values_of_neighbors.append(Grain_grow.image[row+1, col])
+            self.values_of_neighbors.append(Grain_grow.image_old[row+1, col])
 
         if (col-1) < 0: #left
             pass
         else: 
-            self.values_of_neighbors.append(Grain_grow.image[row, col-1])
+            self.values_of_neighbors.append(Grain_grow.image_old[row, col-1])
 
         if (col+1) > Grain_grow.width-2: #GeT_RiGhT
             pass
         else: 
-            self.values_of_neighbors.append(Grain_grow.image[row, col+1])
-
-        # print(self.values_of_neighbors)
-
-
+            self.values_of_neighbors.append(Grain_grow.image_old[row, col+1])
 
         try:
             value = int(mode(self.values_of_neighbors))
             if value == 0:
                 value = max(self.values_of_neighbors)
         
-
         except StatisticsError:
             value = int(max(self.values_of_neighbors)) 
 
@@ -104,4 +114,3 @@ class Rules:
 
 if __name__ == '__main__':
     Grain_grow()
-
